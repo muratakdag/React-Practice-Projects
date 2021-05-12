e907694049f7af8a8e97d15b17ed1e0fde9112f0
muratakdag <75887798+muratakdag@users.noreply.github.com>
2021-05-12 12:01:59 +0300

    Add event handlers

--- Youtube Browser/src/components/App.js
+++ Youtube Browser/src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import SearchBar from './SearchBar';
+import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
       </div>
     );
