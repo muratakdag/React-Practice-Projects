e42bc7954355bf93dc1b9cbd4bc352f5976b8104
muratakdag <75887798+muratakdag@users.noreply.github.com>
2021-05-12 12:13:31 +0300

    Handle form submittal

--- Youtube Browser/src/components/SearchBar.js
+++ Youtube Browser/src/components/SearchBar.js
@@ -3,12 +3,19 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onInputChange = () => {};
+  onInputChange = (event) => {
+    const userInput = event.target.value;
+    this.setState({ term: userInput });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input
