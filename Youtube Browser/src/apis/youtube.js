8d4696726f4c0f208e6fa4f9d87c9040efd9d605
muratakdag <75887798+muratakdag@users.noreply.github.com>
2021-05-12 17:20:30 +0300

    Add search videos

--- Youtube Browser/src/apis/youtube.js
+++ Youtube Browser/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBR6TGNS6EGtQhlrs5mSyigU5iQXBnicSg";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
