ce7d53d7a1fad66a1f27bcd6345fd494e98d19e4
muratakdag <75887798+muratakdag@users.noreply.github.com>
2021-05-17 12:42:05 +0300

    Style the VideoDetail

--- Youtube Browser/src/components/VideoItem.js
+++ Youtube Browser/src/components/VideoItem.js
@@ -5,7 +5,7 @@ import VideoList from "./VideoList";
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={video.snippet.thumpnails.medium.url} />
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
